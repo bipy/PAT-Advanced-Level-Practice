6164da35ab1e30d1d8d88b2b1b1ffff164982f39
睿涵 郑 <zhengrh1@outlook.com>
2019-06-05 14:30:37 +0800

    add

---
+++ 1001 A+B Format/源.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a, b, sum;
+	cin >> a >> b;
+	sum = a + b;
+	if (sum < 0) {
+		printf("-");
+		sum = -sum;
+	}
+	int p = 1;
+	while (sum / (p * 1000) > 0) {
+		p *= 1000;
+	}
+	printf("%d", (sum / p) % 1000);
+	p /= 10;
+	int count = 2;
+	while (p >= 1) {
+		if (++count == 3 && p != 1) {
+			printf(",");
+			count = 0;
+		}
+		printf("%d", (sum / p) % 10);
+		p /= 10;
+	}
+	return 0;
+}
---
+++ 1002 A+B for Polynomials/源.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+typedef struct Lnode* List;
+struct Lnode {
+	double c;
+	double e;
+	List next;
+};
+int main() {
+	
+}

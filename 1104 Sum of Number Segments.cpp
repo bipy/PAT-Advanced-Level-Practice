d32f7b665daca9906a5da2335f6532e948fdf0b0
bipy <876563776@qq.com>
2020-06-16 17:55:10 +0800

    source code

--- 1104 Sum of Number Segments.cpp
+++ 1104 Sum of Number Segments.cpp
@@ -1,13 +1,20 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main() {
-	int N;
-	cin >> N;
-	double v, ans = 0;
-	for (int i = 1; i <= N; i++) {
-		scanf("%lf", &v);
-		ans += v * i * (N - i + 1);
-	}
-	printf("%.2f", ans);
-	return 0;
+    int N;
+    cin >> N;
+    vector<long long> seq(N + 1);
+    double temp;
+    for (int i = 1; i <= N; i++) {
+        scanf("%lf", &temp);
+        seq[i] = temp * 1000;
+    }
+    long long sum = seq[N];
+    for (int i = N - 1; i > 0; i--) {
+        seq[i] = seq[i + 1] + seq[i] * (N - i + 1);
+        sum += seq[i];
+    }
+    printf("%.2f", sum / 1000.0);
+    return 0;
 }

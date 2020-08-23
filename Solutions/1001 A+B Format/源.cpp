9d801ec55da15e6de923281a77bb87336e539025
bipy <876563776@qq.com>
2020-08-23 17:31:34 +0800

    Update

--- Solutions/1001 A+B Format/源.cpp
+++ Solutions/1001 A+B Format/源.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include <string>
 using namespace std;
-//以1000为步长找到开头并输出后，后面要一位一位地输出，否则“000”只能输出一个“0”
 int main() {
 	int a, b, sum;
 	cin >> a >> b;
@@ -10,7 +9,6 @@ int main() {
 		printf("-");
 		sum = -sum;
 	}
-	//字符串方式处理更简单明了
 	string s = to_string(sum);
 	int len = s.length();
 	for (int i = 0; i < len; i++) {
@@ -19,23 +17,5 @@ int main() {
 			printf(",");
 		}
 	}
-	//以下为数字方式AC
-	/*
-		int p = 1;
-		while (sum / (p * 1000) > 0) {
-			p *= 1000;
-		}
-		printf("%d", (sum / p) % 1000);
-		p /= 10;
-		int count = 2;
-		while (p >= 1) {
-			if (++count == 3 && p != 1) {
-				printf(",");
-				count = 0;
-			}
-			printf("%d", (sum / p) % 10);
-			p /= 10;
-		}
-	*/
 	return 0;
 }
